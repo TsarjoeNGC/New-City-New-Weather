84eeaa5a66a90f03c359fdf2928b39ef9ce6b42f
joseph akah <josephakah@macbook-pro-2.lan>
2023-02-06 09:48:56 +0000

    third commit files of html css javascript

--- assets/script.js
+++ assets/script.js
@@ -18,8 +18,52 @@ var key = "dfd0a4d90e0b02b5591aeb3c59a3f119";
 
 init();
 
+function init() {
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
 
-$(document).on("click", "#listC", function () {
-    var thisCity = $(this).attr("data-city");
-    getResponseWeather(thisCity);
+    if (storedCities !== null) {
+        cities = storedCities;
+    }
+    renderCities();
+}
+
+function storeCities() {
+    localStorage.setItem("cities", JSON.stringify(cities));
+    console.log(localStorage);
+}
+
+function renderCities() {
+    cityList.empty();
+
+    for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];
+
+        var li = $("<li>").text(city);
+        li.attr("id", "listC");
+        li.attr("data-city", city);
+        li.attr("class", "list-group-item");
+        console.log(li);
+        cityList.prepend(li);
+    }
+    if (!city) {
+        return
+    }
+    else {
+        getResponseWeather(city)
+    };
+}
+
+$("#add-city").on("click", function (event) {
+    event.preventDefault();
+
+    var city = $("#city-input").val().trim();
+
+    if (city === "") {
+        return;
+    }
+    cities.push(city);
+    storeCities();
+    renderCities();
 });
+
+
